8b5415c085b96821dd59c36d427b72c0310d87d1
Ryuju0911 <pinebook0911@gmail.com>
2023-09-06 20:10:52 +0900

    Add consistent error handling middleware.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,15 +2,16 @@ import express from 'express';
 import { json } from 'body-parser';
 
 import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/singin';
-import { signoutRotuer } from './routes/signout';
 import { signupRouter } from './routes/signup';
+import { errorHandler } from './middleware/error-handler';
 
 const app = express();
 app.use(json());
 
 app.use(currentUserRouter);
 app.use(signupRouter);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
