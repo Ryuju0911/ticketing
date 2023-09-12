0591388531efcf5b7e58c59edfbc4c8ddd1b7ccc
Ryuju0911 <pinebook0911@gmail.com>
2023-09-12 15:32:48 +0900

    Generate JSON Web Token for authenticated users, and stor it into Cookie.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signupRouter } from './routes/signup';
@@ -9,7 +10,14 @@ import { errorHandler } from './middleware/error-handler';
 import { NotFoundError } from './errors/not-found';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+)
 
 app.use(currentUserRouter);
 app.use(signupRouter);
@@ -21,6 +29,10 @@ app.all('*', async (req, res) => {
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+
   try {
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
   } catch (err) {
