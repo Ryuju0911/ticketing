91759d6ca3cb58d0864b3ef46ddb42cd2fea32e7
Ryuju0911 <pinebook0911@gmail.com>
2023-09-13 22:03:00 +0900

    Add supertest and jest packages, and test signup API with a valid request.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,36 +1,6 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRotuer } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middleware/error-handler';
-import { NotFoundError } from './errors/not-found';
-
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-)
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRotuer);
-app.use(signupRouter);
-
-app.all('*', async (req, res) => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -42,8 +12,6 @@ const start = async () => {
   } catch (err) {
     console.error(err);
   }
-
-  console.log("Connected to MongoDB")
   
   app.listen(3000, () => {
     console.log("Listening on port 3000");
