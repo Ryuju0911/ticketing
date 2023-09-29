8cff40c4812e0557630c3122db3011bc1640771d
Ryuju0911 <pinebook0911@gmail.com>
2023-09-29 21:10:29 +0900

    Add auth helper function.

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -2,13 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it('returns a 201 on successful signup', async () => {
-  return request(app)
-    .post('/api/users/signup')
-    .send({
-      email: 'test@test.com',
-      password: 'password',
-    })
-    .expect(201);
+  await global.signup();
 });
 
 it('returns a 400 with an invalid email', async () => {
@@ -48,13 +42,7 @@ it('returns a 400 with missing email and password', async () => {
 });
 
 it('disallows duplicate emais', async () => {
-  await request(app)
-    .post('/api/users/signup')
-    .send({
-      email: 'test@test.com',
-      password: 'password'
-    })
-    .expect(201);
+  await global.signup();
 
   await request(app)
     .post('/api/users/signup')
@@ -66,13 +54,7 @@ it('disallows duplicate emais', async () => {
 });
 
 it('sets a cookie after successful signup', async () => {
-  const response = await request(app)
-    .post('/api/users/signup')
-    .send({
-      email: 'test@test.com',
-      password: 'password'
-    })
-    .expect(201);
+  const response = await global.signup();
 
-  expect(response.get('Set-Cookie')).toBeDefined();
+  expect(response).toBeDefined();
 });
