15ed459d6a0bfa4a52d65fc2c982a258ca6332a2
Ryuju0911 <pinebook0911@gmail.com>
2023-10-13 00:35:09 +0900

    Make update route handler to emit ticketUpdated event.

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
 import { NotAuthroizedError, NotFoundError, requireAuth, validateRequest } from '@ryuju-ticketing/common';
+import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -29,6 +31,13 @@ router.put(
       price: req.body.price,
     });
     await ticket.save();
+
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.send(ticket);
 });
