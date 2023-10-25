ccdbea94d80a463033543d198ce37a43113dcce3
Ryuju0911 <pinebook0911@gmail.com>
2023-10-25 16:22:53 +0900

    Add payment logic using stripe API.

---
+++ payments/src/routes/new.ts
@@ -0,0 +1,53 @@
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import {
+  BadRequestError,
+  NotAuthroizedError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest
+} from '@ryuju-ticketing/common';
+import { stripe } from '../stripe';
+import { Order } from '../models/order';
+
+const router = express.Router();
+
+router.post(
+  '/api/payments',
+  requireAuth,
+  [
+    body('token')
+      .not()
+      .isEmpty(),
+    body('orderId')
+      .not()
+      .isEmpty()
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { token, orderId } = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthroizedError();
+    }
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError('Cannot pay for an cancelled order');
+    }
+
+    await stripe.charges.create({
+      currency: 'usd',
+      amount: order.price * 100,
+      source: token
+    });
+    
+    res.send({ success: true });
+  }
+);
+
+export { router as createChargeRouter }
