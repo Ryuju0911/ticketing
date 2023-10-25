aa20c7ce12f4d163c13fa5c4a3ddfdf1066ba72c
Ryuju0911 <pinebook0911@gmail.com>
2023-10-25 13:10:43 +0900

    Introduce the payments service.

---
+++ payments/src/app.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@ryuju-ticketing/common';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    // secure must be false during tests becasue we are making
+    // plain HTTP request when making use of supertest.
+    secure: process.env.NODE_ENV !== 'test',
+  })
+)
+app.use(currentUser);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
