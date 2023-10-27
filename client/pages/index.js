06088fb71baa24f8819ea807a42b18df009dad42
Ryuju0911 <pinebook0911@gmail.com>
2023-10-27 14:20:42 +0900

    Add client pages corresponding to the tickets, orders, payments, and expiration service.

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,37 @@
-import buildClient from "../api/build-client";
+import Link from 'next/link';
 
-const LandingPage = ({ currentUser }) => {
-  return currentUser? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>;
+const LandingPage = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map(ticket => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+        <td>
+          <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
+            View
+          </Link>
+        </td>
+      </tr>
+    );
+  });
+
+  return (
+    <dive>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>
+          {ticketList}
+        </tbody>
+      </table>
+    </dive>
+  );
 };
 
 // getInitialProps is executed either on the server or on the client.
@@ -13,11 +43,10 @@ const LandingPage = ({ currentUser }) => {
 //
 // Executed on the client
 //  * Navigating from one page to another while in the app
-LandingPage.getInitialProps = async (context) => {
-  const client = buildClient(context);
-  const { data } = await client.get('/api/users/currentuser');
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  const { data } = await client.get('/api/tickets');
 
-  return data;
+  return { tickets: data };
 };
 
 export default LandingPage;
